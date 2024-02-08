f0f22f0a1c07863516df05f3796084931bd573e9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-02-08 15:39:48 +0100

    dev: setex

--- api.js
+++ api.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const axios = require("axios").default;
 
+const redis = require("redis");
+
+const { createClient } = redis;
+
+let client;
+(async () => {
+  client = await createClient()
+    .on("error", (err) => console.log("Redis Client Error", err))
+    .connect();
+})();
+
 const app = express.Router();
 
 const url = "https://api.github.com/users";
@@ -8,9 +19,17 @@ const url = "https://api.github.com/users";
 app.get("/users", (req, res) => {
   (async () => {
     console.time();
-    const result = await axios.get(url);
+    const value = await client.get(url);
+    if (value === null) {
+      const result = await axios.get(url);
+      await client.set(url, JSON.stringify(result.data), { EX: 10 });
+      console.timeEnd();
+      res.json(result.data);
+      return;
+    }
+    const data = JSON.parse(value);
     console.timeEnd();
-    res.json(result.data);
+    res.json(data);
   })();
 });
 
